ade1f5631e8e44b4ab8af8bb3b1b6d67bb75660e
Hammish48 <Hamlennane@gmail.com>
2024-03-23 13:13:19 +1100

    stuff

--- game.py
+++ game.py
@@ -29,6 +29,34 @@ class Turn:
     def __init__(self, x, y, directions:Vec4Bool) -> None:
         self.position = pg.Vector2(x, y)
         self.directions = directions
+        self.texture:pg.surface.Surface
+        self.texture = pg.image.load("./assets/pipes/two ends.png").convert()
+        count:int = 0
+        count += int(directions.w)
+        count += int(directions.x)
+        count += int(directions.y)
+        count += int(directions.z)
+        match (count):
+            case 1:
+                self.texture = pg.image.load("./assets/pipes/dead end.png").convert_alpha()
+            case 2:
+                self.texture = pg.image.load("./assets/pipes/two ends corner.png").convert_alpha()
+                if directions.w:
+                    self.texture = pg.transform.rotate(self.texture, 180)
+                    if directions.x:
+                        self.texture = pg.transform.flip(self.texture, True, False)
+                elif directions.z:
+                    self.texture = pg.transform.flip(self.texture, True, False)
+            case 3:
+                self.texture = pg.image.load("./assets/pipes/three ends.png").convert_alpha()
+                if directions.x and directions.z:
+                    self.texture = pg.transform.rotate(self.texture, 90)
+                    if directions.y:
+                        self.texture = pg.transform.flip(self.texture, False, True)
+                elif directions.z:
+                    self.texture = pg.transform.flip(self.texture, True, False)
+            case 4:
+                self.texture = pg.image.load("./assets/pipes/4 way joiner.png").convert()
 
 
 def checkCollisionRecs(r1:pg.Rect, r2:pg.Rect) -> bool:
@@ -37,16 +65,22 @@ def checkCollisionRecs(r1:pg.Rect, r2:pg.Rect) -> bool:
     return False
 
 
-def main():
-    screen = pg.display.set_mode((1220, 720))
-    pg.display.set_caption("Pyweek 37")
+def DrawPipe(screen, p1:pg.Vector2, p2:pg.Vector2):
+    if p1.x == p2.x:
+        print("vertical pipe")
+    else:
+        print("horizontal pipe")
+
+
+def game(screen_, lvl):
+    screen:pg.Surface = screen_
     fps = pg.time.Clock()
     pen = pg.draw
     clickCooldown = True
     food = Food(200,200)
     fish = Fish(500, 300, True,(45, 45, 90))
     #turn format is: up, right, down, left    clockwise openings
-    turns = [Turn(300, 300, Vec4Bool(True, True, True, False)), Turn(300, 500, Vec4Bool(True, True, False, False))]
+    turns = [Turn(300, 300, Vec4Bool(False, True, True, False)), Turn(300, 500, Vec4Bool(True, True, False, True)), Turn(800, 500, Vec4Bool(True, False, True, True)), Turn(800, 300, Vec4Bool(False, True, True, True))]
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT:
@@ -78,14 +112,14 @@ def main():
             if checkCollisionRecs(pg.Rect(fish.position.x, fish.position.y, orientation.x, orientation.y), pg.Rect(turn.position.x, turn.position.y, 50, 50)):
                 if turn.directions.w:
                     #print("up")
-                    if (fish.direction) and checkCollisionRecs(pg.Rect(fish.position.x, fish.position.y, 15, orientation.y), pg.Rect(turn.position.x, turn.position.y, 50, 50)) and checkCollisionRecs(pg.Rect(fish.position.x + 35, fish.position.y, 15, orientation.y), pg.Rect(turn.position.x, turn.position.y, 50, 50)) and checkCollisionRecs(pg.Rect(fish.position.x - 20, fish.position.y - 180, 90, 390), pg.Rect(food.position.x, food.position.y, 30, 30))and not checkCollisionRecs(fish.foodBounds, pg.Rect(food.position.x, food.position.y, 30, 30)):
+                    if (fish.direction) and checkCollisionRecs(pg.Rect(fish.position.x, fish.position.y, 20, orientation.y), pg.Rect(turn.position.x, turn.position.y, 50, 50)) and checkCollisionRecs(pg.Rect(fish.position.x + 30, fish.position.y, 20, orientation.y), pg.Rect(turn.position.x, turn.position.y, 50, 50)) and checkCollisionRecs(pg.Rect(fish.position.x - 20, fish.position.y - 180, 90, 390), pg.Rect(food.position.x, food.position.y, 30, 30))and not checkCollisionRecs(fish.foodBounds, pg.Rect(food.position.x, food.position.y, 30, 30)):
                         fish.direction = False
                         fish.position.x = turn.position.x + 20
                 elif not fish.direction:
                     if fish.position.y < turn.position.y:
                         fish.position.y = turn.position.y
                 if turn.directions.x:
-                    if (not fish.direction) and checkCollisionRecs(pg.Rect(fish.position.x, fish.position.y, 20, 15), pg.Rect(turn.position.x, turn.position.y, 50, 50)) and checkCollisionRecs(pg.Rect(fish.position.x, fish.position.y+35, 20, 15), pg.Rect(turn.position.x, turn.position.y, 50, 50)) and checkCollisionRecs(pg.Rect(fish.position.x - 180, fish.position.y - 20, 390, 90), pg.Rect(food.position.x, food.position.y, 30, 30))and not checkCollisionRecs(fish.foodBounds, pg.Rect(food.position.x, food.position.y, 30, 30)):
+                    if (not fish.direction) and checkCollisionRecs(pg.Rect(fish.position.x, fish.position.y, 20, 20), pg.Rect(turn.position.x, turn.position.y, 50, 50)) and checkCollisionRecs(pg.Rect(fish.position.x, fish.position.y+30, 20, 20), pg.Rect(turn.position.x, turn.position.y, 50, 50)) and checkCollisionRecs(pg.Rect(fish.position.x - 180, fish.position.y - 20, 390, 90), pg.Rect(food.position.x, food.position.y, 30, 30))and not checkCollisionRecs(fish.foodBounds, pg.Rect(food.position.x, food.position.y, 30, 30)):
                         fish.direction = True
                         fish.position.y = turn.position.y + 20
                 elif fish.direction:
@@ -93,7 +127,7 @@ def main():
                         fish.position.x = turn.position.x + 10
                 if turn.directions.y:
                     #print("down")
-                    if (fish.direction) and checkCollisionRecs(pg.Rect(fish.position.x, fish.position.y, 15, orientation.y), pg.Rect(turn.position.x, turn.position.y, 50, 50)) and checkCollisionRecs(pg.Rect(fish.position.x + 35, fish.position.y, 15, orientation.y), pg.Rect(turn.position.x, turn.position.y, 50, 50)) and checkCollisionRecs(pg.Rect(fish.position.x - 20, fish.position.y - 180, 90, 390), pg.Rect(food.position.x, food.position.y, 30, 30)) and not checkCollisionRecs(fish.foodBounds, pg.Rect(food.position.x, food.position.y, 30, 30)):
+                    if (fish.direction) and checkCollisionRecs(pg.Rect(fish.position.x, fish.position.y, 20, orientation.y), pg.Rect(turn.position.x, turn.position.y, 50, 50)) and checkCollisionRecs(pg.Rect(fish.position.x + 30, fish.position.y, 20, orientation.y), pg.Rect(turn.position.x, turn.position.y, 50, 50)) and checkCollisionRecs(pg.Rect(fish.position.x - 20, fish.position.y - 180, 90, 390), pg.Rect(food.position.x, food.position.y, 30, 30)) and not checkCollisionRecs(fish.foodBounds, pg.Rect(food.position.x, food.position.y, 30, 30)):
                         fish.direction = False
                         fish.position.x = turn.position.x + 20
                         print(fish.direction)
@@ -102,7 +136,7 @@ def main():
                         print("aaaa")
                         fish.position.y = turn.position.y + 10
                 if turn.directions.z:
-                    if (not fish.direction) and checkCollisionRecs(pg.Rect(fish.position.x, fish.position.y, 20, 15), pg.Rect(turn.position.x, turn.position.y, 50, 50)) and checkCollisionRecs(pg.Rect(fish.position.x, fish.position.y+35, 20, 15), pg.Rect(turn.position.x, turn.position.y, 50, 50)) and checkCollisionRecs(pg.Rect(fish.position.x - 180, fish.position.y - 20, 390, 90), pg.Rect(food.position.x, food.position.y, 30, 30))and not checkCollisionRecs(fish.foodBounds, pg.Rect(food.position.x, food.position.y, 30, 30)):
+                    if (not fish.direction) and checkCollisionRecs(pg.Rect(fish.position.x, fish.position.y, 20, 20), pg.Rect(turn.position.x, turn.position.y, 50, 50)) and checkCollisionRecs(pg.Rect(fish.position.x, fish.position.y+30, 20, 20), pg.Rect(turn.position.x, turn.position.y, 50, 50)) and checkCollisionRecs(pg.Rect(fish.position.x - 180, fish.position.y - 20, 390, 90), pg.Rect(food.position.x, food.position.y, 30, 30))and not checkCollisionRecs(fish.foodBounds, pg.Rect(food.position.x, food.position.y, 30, 30)):
                         fish.direction = True
                         fish.position.y = turn.position.y + 20
                 elif fish.direction:
@@ -114,7 +148,7 @@ def main():
         #drawing
         screen.fill((255, 255, 255))
         for t in turns:
-            pen.rect(screen, (200, 0, 200), pg.Rect(t.position.x, t.position.y, 60, 60))
+            screen.blit(t.texture, (t.position.x, t.position.y))
         if fish.direction:
             #pen.rect(screen, (0, 255, 0), fish.foodBounds)
             #pen.rect(screen, (255, 20, 239), pg.Rect(fish.position.x - 20, fish.position.y - 180, 90, 390))
@@ -127,6 +161,11 @@ def main():
         pen.rect(screen, (0,255,0), pg.Rect(pg.mouse.get_pos()[0], pg.mouse.get_pos()[1], 2, 2))
         fps.tick(60)
         pg.display.update()
+
+def main():
+    screen = pg.display.set_mode((1220, 720))
+    pg.display.set_caption("Pyweek 37")
+    game(screen, 0)
 
 if __name__ == "__main__":
     main()
